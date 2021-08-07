838c4e60181bf87d566882d83ceb0b3e2f376a3c
Alexander Moskalevich <sashamoskalevich0217@gmail.com>
2021-08-07 18:55:55 +0300

    Initial commit

--- locations/farm.py
+++ locations/farm.py
@@ -1,3 +1,20 @@
+from bot import bot
+
 
 def process_message(message, user, bot):
-    bot.send_message(user['id'], "Вы на ферме!")
+    bot.send_message(user['id'],
+                     "Вы на ферме! Здесь можно выращивать овощи а огороде, собирать цветы в саду, и разводить скот в загонах для последущей продажи в магазине(/goto shop). Внимание! Собирайте вовремя урожай, иначе он сгниет, и вы потеряете готубли./n"
+                     "огород - /garden /n"
+                     "cад - /flowers /n"
+                     "загон - /paddock /n")
+
+
+@bot.message_handler(commands=["garden"])
+def repeat_all_messages(message):
+    pass
+@bot.message_handler(commands=["flowers"])
+def repeat_all_messages(message):
+    pass
+@bot.message_handler(commands=["paddock"])
+def repeat_all_messages(message):
+    pass
