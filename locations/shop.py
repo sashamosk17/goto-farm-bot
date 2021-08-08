ea9122f54d33ea65445f30ab099191e7c3046cf7
MatveyKryl <matveykryl101006@gmail.com>
2021-08-08 18:09:42 +0300

    bank почти работает (но не работает)

--- locations/shop.py
+++ locations/shop.py
@@ -7,28 +7,34 @@ def send_menu(chat_id, bot, helpers):
     buttons = ["Полочка 'Всё для животных'", "Полочка 'Всё для растений'", "В углу стоит подозрительный гном",
                "Дверь. Ведёт в подвал. Наверное..."]
     keyboard = helpers.generate_keyboard(buttons)
-    bot.send_message(chat_id, "Чего хотите?", reply_markup=keyboard)
+    bot.send_message(chat_id, "Вы вошли в магазин", reply_markup=keyboard)
+
+def exchange(message, user, bot):
+    if any['1000', '3000', '5000', '15000'] + " монет в обмен на " + any['50', '120', '199', '400'] + " готублей" == message:
+        bot.send_message(user['id'], 'Введите "/donate "НОМЕР_СЧЁТА"" без внутренних каывчек')
+        user['gived_money'] = int(message.split()[2])
+        user['wanted_money'] = int(message.split()[0])
+    if "/donate" in message:
+        user['nomer_shota'] = message.split(' ')[1]
+        bank.ask_money(user['nomer_shota'], user['gived_money'], "Пользователь поукпает внутреигровую (Весёлая ферма:Возрождение) валюту за {} готублей".format(str(user['wanted_money'])), )
+        bot.send_message(user['id'], 'Введите "/verify_donate "КОД_ПОДТВЕРЖДЕНИЯ"" без внутренних кавычек')
+    if "/verify_donate" in message:
+        bank.verify_transaction(user['nomer_shota'], message.split[1])
+        user['balance'] += user['wanted_money']
 
 def process_message(message, user, bot, helpers):
     send_menu(user['id'], bot)
-
+    exchange(message, user, bot)
     if message == "Полочка 'Всё для животных'":
         pass
     if message == "Полочка 'Всё для растений'":
         pass
     if message == "В углу стоит подозрительный гном":
-        bot.send_message(user['id'], "Он предлагает мне мешочек с...")
-        buttons = ["1000 монет в обмен на 50 готублей", "3000 монет в обмен на 120 готублей","5000 монет в обмен на 200 готублей","15000 монет в обмен на 400 готублей", "Назад"]
+        buttons = ["1000 монет в обмен на 50 готублей", "3000 монет в обмен на 120 готублей","5000 монет в обмен на 199 готублей","15000 монет в обмен на 400 готублей", "Назад"]
         keyboard = helpers.generate_keyboard(buttons)
+        bot.send_message(user['id'], "Он предлагает мне мешочек с...", reply_markup=keyboard)
         #bank.ask_money(НОМЕР СЧЕТА ПОКУПАТЕЛЯ, СУММА, 'Пользователь обменивает готубли на внутреигровую валюту игры"Весёлая ферма: Возрождение"')
         #bank.verify_transaction(НОМЕРСЧЕТАПОКУПАТЕЛЯ, КОДПОДТВЕРЖДЕНИЯ)
         #send_money(НОМЕР СЧЕТА ПОКУПАТЕЛЯ, СУММА, "ОПИСАНИЕ ПЕРЕВОДА")
     if message == "Дверь. Ведёт в подвал. Наверное...":
         bot.send_message(user, "Я попал в казино")
-
-
-
-
-
-
-
